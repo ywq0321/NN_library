69dadd20a4ca9ed89a9a93df1b9aa123921fc58b
Alex Yi <ywq0321@gmail.com>
2024-01-11 21:17:10 +0000

    main

--- main.py
+++ main.py
@@ -3,26 +3,40 @@ from random import random
 
 
 class Layer:
-    def __init__(self, width, out):
+    def __init__(self, width, out, acti='ReLU'):
         self.width = width
         self.out = out
+        self.acti = acti
         self.value = np.array([None for i in range(width)])
-        self.weights = np.array([[random() for i in range(out)] for i in range(width)])
-        self.bias = np.array([random() for i in range(out)])
+        self.weights = np.array([[random()*2-1 for i in range(out)] for i in range(width)])
+        self.bias = np.array([random()*2-1 for i in range(out)])
 
     def __call__(self, *args, **kwargs):  # get output
-        return np.dot(self.value, self.weights) + self.bias
+        return self.activation(np.dot(self.value, self.weights) + self.bias, self.acti)
+
+    def activation(self, x, func):
+        if func == 'ReLU':
+            for i in range(len(x)):
+                x[i] = x[i] if x[i] > 0 else 0
+        elif func == 'tanh':
+            for i in range(len(x)):
+                x[i] = np.tanh(x[i])
+        elif func == 'sigmoid':
+            for i in range(len(x)):
+                x[i] = 1 / (1 + np.exp(-x[i]))
+        return x
 
 
-class NN:
+class NN:  # creates a NN with ReLU activation except the last layer, which has sigmoid
     def __init__(self, depth: int, widths: list):
         assert len(widths) == depth+1
         self.depth = depth
         self.widths = widths
 
         self.network = []
-        for i in range(depth):
+        for i in range(depth-1):
             self.network.append(Layer(widths[i], widths[i+1]))
+        self.network.append(Layer(widths[depth-1], widths[depth], acti='sigmoid'))
 
     def forward_propagate(self, inputs: list):
         assert len(inputs) == self.widths[0]
@@ -38,4 +52,4 @@ class NN:
 
 
 myNN = NN(3, [8, 4, 2, 1])
-print(myNN.forward_propagate([i for i in range(8)]))
+print(myNN.forward_propagate([i for i in range(8)])[0])
