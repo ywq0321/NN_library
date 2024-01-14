8de8d2769ca75edd2d15c9781a8a07ad543a011a
Alex Yi <ywq0321@gmail.com>
2024-01-14 12:20:50 +0000

    main

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ import numpy as np
 
 
 class DenseLayer:
-    def __init__(self, width, out, activation='ReLU'):
+    def __init__(self, width, out, acti='tanh'):
         self.width = width
         self.out = out
-        self.activation = activation
+        self.acti = acti
         self.value = np.array([None for i in range(width)])
         self.weights = np.array([[np.random.rand() * 2 - 1 for i in range(out)] for i in range(width)])
         self.bias = np.array([np.random.rand() * 2 - 1 for i in range(out)])
 
-    def __call__(self):  # get output
-        return self.activation(np.dot(self.value, self.weights) + self.bias, self.activation)
+    def __call__(self, *args, **kwargs):  # get output
+        return self.activation(np.dot(self.value, self.weights) + self.bias, self.acti)
 
     def activation(self, x, func):
         if func == 'ReLU':
@@ -27,15 +27,14 @@ class DenseLayer:
 
 
 class NN:  # creates a NN with ReLU activation except the last layer, which has sigmoid
-    def __init__(self, depth: int, widths: list):
-        assert len(widths) == depth + 1
-        self.depth = depth
+    def __init__(self, widths: list):
+        self.depth = len(widths) - 1
         self.widths = widths
 
         self.network = []
-        for i in range(depth - 1):
+        for i in range(self.depth - 1):
             self.network.append(DenseLayer(widths[i], widths[i + 1]))
-        self.network.append(DenseLayer(widths[depth - 1], widths[depth], activation='sigmoid'))
+        self.network.append(DenseLayer(widths[self.depth - 1], widths[self.depth], acti='sigmoid'))
 
     def forward_propagate(self, inputs: list):
         assert len(inputs) == self.widths[0]
@@ -52,31 +51,49 @@ class NN:  # creates a NN with ReLU activation except the last layer, which has 
         return sum / len(outputs)
 
     def MSE_dash(self, outputs, labels):
-        sum = 0
+        sum = []
         for i in range(len(outputs)):
-            sum += np.abs(outputs[i] - labels[i])
-        return sum * 2 / len(outputs)
+            sum.append(2 * np.abs(outputs[i] - labels[i]))
+        return sum
+
+    def transpose(self, x):
+        x = np.asarray(x)
+        if len(x.shape) == 1:
+            x = x.reshape(x.shape[0], 1)
+        else:
+            x = np.transpose(x)
+        return x
 
     def back_propagate(self, error):
-        learning_rate = 0.01
+        error = np.array(error)
+        learning_rate = 0.1
 
         for i in range(len(self.network) - 1, -1, -1):
-            input_error = np.dot(error, self.network[i].weights)
-            self.network[i].weights -= learning_rate * np.dot(self.network[i].value, error)
+            input_error = np.dot(error, self.transpose(self.network[i].weights))
+            self.network[i].weights -= self.transpose(learning_rate * np.dot(self.network[i].value.reshape(self.widths[i], 1),
+                                                                             error.reshape(1, self.widths[i+1])))
             self.network[i].bias -= learning_rate * error
             error = input_error
 
     def train(self, epochs: int, inputs: list, labels: list):
         assert len(inputs) == len(labels)
-        assert len(inputs) == self.widths[0]
+        assert len(inputs[0]) == self.widths[0]
         for i in range(epochs):
-            print('Epoch:', i)
+            print('Epoch:', i+1)
+            error = 0
+            error_dash = [0 for i in range(len(labels[0]))]
             for j in range(len(inputs)):
                 predicted = self.forward_propagate(inputs[j])
-                error = self.MSE(predicted, labels[i])
-                error_dash = self.MSE_dash(predicted, labels[i])
-                print('Error:', error)
-                self.back_propagate(error_dash)
+                error += self.MSE(predicted, labels[j])
+                err = self.MSE_dash(predicted, labels[j])
+                for i in range(len(err)):
+                    error_dash[i] += err[i]
+            print('Error:', error)
+            self.back_propagate(error_dash)
+            # print(self.forward_propagate([1, 1]))
+            # print(self.forward_propagate([1, 0]))
+            # print(self.forward_propagate([0, 1]))
+            # print(self.forward_propagate([0, 0]))
 
 
 def square(x):
@@ -87,7 +104,10 @@ def generate_data(func, min, max, size):
     x = np.random.uniform(min, max, size)
     y = []
     for i in x:
-        y.append(func(x))
-    return y
+        y.append([func(i)])
+    return x.reshape((size, 1)), y
 
 
+# x, y = generate_data(square, -10, 10, 20)
+my_NN = NN([2, 2, 2])
+my_NN.train(100, [[1, 1], [1, 0], [0, 1], [0, 0]], [[1, 1], [1, 0], [0, 1], [0, 0]])
