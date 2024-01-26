a8f96ddddb8c5dfb45a70344ef38f8cdb27f533a
Alex <ywq0321@gmail.com>
2024-01-26 20:57:39 +0000

    Update main.py

--- scalar.py
+++ scalar.py
@@ -8,7 +8,7 @@ class Value:
         else:
             raise ValueError('int or float expected, but ' + str(type(value)) + ' is given. ')
         self.parents = parents
-        self.gradient = 1
+        self.gradient = 0
 
     def __add__(self, other):
         other = other if type(other) == Value else Value(other)
@@ -44,16 +44,16 @@ class Value:
         return other * self ** -1
 
     def relu(self):
-        return self.value if self.value > 0 else -self.value
+        return self.value if self.value > 0 else 0
 
     def backward(self, cur=None, visited=None):
         if visited is None:
             visited = []
-        if self.parents == ():
-            print(self.gradient)
-            return
         if cur is None:
             cur = self
+            self.gradient = 1
+        if cur.parents == ():
+            return
 
         # find d(out) / d(self) and d(out) / d(other)
         if cur.parents[2] == 'add':
@@ -76,16 +76,10 @@ class Value:
 a = Value(-4.0)
 b = Value(2.0)
 c = a + b
-d = a * b + b ** 3
-c += c + 1
-c += 1 + c + (-a)
-d += d * 2 + (b + a).relu()
-d += 3 * d + (b - a).relu()
-e = c - d
-f = e ** 2
-g = f / 2.0
-g += 10.0 / f
-print(f'{g.value:.4f}') # prints 24.7041, the outcome of this forward pass
-g.backward()
-print(f'{a.gradient:.4f}') # prints 138.8338, i.e. the numerical value of dg/da
-print(f'{b.gradient:.4f}') # prints 645.5773, i.e. the numerical value of dg/db
+d = b**3
+e = a*b
+f = e+d
+f.backward()
+print(a.gradient)
+print(b.gradient)
+print(e.gradient)
